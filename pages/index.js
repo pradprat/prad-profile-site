258de2456ce386c7b2f34fd6acea2b474d527b53
pradprat <pradikafp14@gmail.com>
2022-02-14 22:38:35 +0700

    add photo

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,25 @@
 import Head from 'next/head';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
-
+import { Flex, HStack, Button } from '@chakra-ui/react';
+import styles from './index.module.scss';
+import photo from '../assets/images/me1.jpg';
 export default function Home() {
     return (
-        <div className='container'>
+        <div className='container' style={{ minHeight: '100vh' }}>
             <Head>
                 <title>Next.js Starter!</title>
                 <link rel='icon' href='/favicon.ico' />
             </Head>
 
             <main>
-                <Header title='Welcome to my app!' />
-                <p className='description'>
-                    Get started by editing <code>pages/index.js</code>
-                </p>
+                <Header />
+                <HStack>
+                    <div className={styles.photo}>
+                        <img src={photo.src}></img>
+                    </div>
+                    <div>Desc</div>
+                </HStack>
             </main>
 
             <Footer />
