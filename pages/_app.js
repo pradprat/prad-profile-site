2f6df3609101951dee09d20a42d95d7f6005f1f7
pradika pratama <pradikafp14@gmail.com>
2022-02-14 11:42:18 +0700

    add header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '@styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import '@styles/globals.css';
+const colors = {
+    brand: {
+        900: '#1a365d',
+        800: '#153e75',
+        700: '#2a69ac',
+    },
+};
 
+const theme = extendTheme({ colors });
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <ChakraProvider>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    );
 }
 
-export default Application
+export default Application;
